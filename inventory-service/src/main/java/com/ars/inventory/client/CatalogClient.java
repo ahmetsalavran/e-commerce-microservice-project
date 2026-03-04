8efbd6b834503a1e490986ebd083eee5ad23d898
a.r.s <ars2743@hotmail.com>
2026-03-04 13:30:35 +0300

    düzenlemeler yapıldı

---
+++ inventory-service/src/main/java/com/ars/inventory/client/CatalogClient.java
@@ -0,0 +1,16 @@
+package com.ars.inventory.client;
+
+import com.ars.contract.catalog.GetProductPricesRequest;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+@FeignClient(name = "inventory-catalog-client", url = "${clients.catalog.url}")
+public interface CatalogClient {
+
+    @PostMapping("/productPrices")
+    Map<Long, BigDecimal> getProductPrices(@RequestBody GetProductPricesRequest request);
+}
